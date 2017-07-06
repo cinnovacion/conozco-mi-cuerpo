34d7760da36442177449613169174bca92d072bc
lindamar <linkl3@hotmail.com>
2017-07-05 18:22:42 -0600

    primer interfaz

--- js/activity.js
+++ js/activity.js
@@ -1,11 +1,31 @@
-define(["sugar-web/activity/activity"], function (activity) {
+"user strict";
+
+define(function (require) {
+    var activity    = require("sugar-web/activity/activity");
+    var datastore   = require("sugar-web/datastore");
+    // Librerias
+    var jquery      = require("jquery");
+    var interact    = require("interact");
+    var Mustache    = require("mustache");
+    // Plantillas
+    var plantilla    = require("../js/plantilla.js");
 
 	// Manipulate the DOM only when it is ready.
 	require(['domReady!'], function (doc) {
 
-		// Initialize the activity.
-		activity.setup();
+        var output   = '';
 
+        // Initialize the activity.
+        activity.setup();
+
+        $('.reload-button').on('click', function() {
+            location.reload();
+        });
+
+        $(document).ready(function(){
+            output = Mustache.render(plantilla[1].pag1, {name: "Luke"});
+            $('#canvas').html(output);
+        });
+    
 	});
-
 });
---
+++ js/plantilla.js
@@ -0,0 +1,12 @@
+define(function (require) {
+    return [
+        {
+            'contenido' : {
+                'pag1' : 'algo mas'
+            }  
+        },
+        {
+            'pag1' : '<div id="pag1"><h1 class="objeto titulo">Seleccione el sistema reproductor<br/> que deseas estudiar</h1><button id="nina" class="objeto boton" type="button"><h3 class="objeto">Femenino</h3></button><button id="nino" class="objeto boton" type="button"><h3 class="objeto">Masculino</h3></button></div>'
+        }
+    ];
+});
