699044077d56094da8e389ff5f8c2a700e54a605
lindamar <linkl3@hotmail.com>
2017-07-10 12:05:37 -0600

    estructura de hasta segundo nivel

--- js/activity.js
+++ js/activity.js
@@ -26,6 +26,16 @@ define(function (require) {
             output = Mustache.render(plantilla[1].pag1, {name: "Luke"});
             $('#canvas').html(output);
         });
+        
+        $('#canvas').on('click','#nina',function(){
+            output = Mustache.render(plantilla[2].pag2);
+            $('#canvas').html(output);
+        });
+        
+        $('#canvas').on('click','#nino',function(){
+            output = Mustache.render(plantilla[3].pag3);
+            $('#canvas').html(output);
+        });
     
 	});
 });
--- js/plantilla.js
+++ js/plantilla.js
@@ -2,11 +2,18 @@ define(function (require) {
     return [
         {
             'contenido' : {
-                'pag1' : 'algo mas'
+                'pag1' : 'algo mas',
+                'pag2' : 'algo mas'
             }  
         },
         {
             'pag1' : '<div id="pag1"><h1 class="objeto titulo">Seleccione el sistema reproductor<br/> que deseas estudiar</h1><button id="nina" class="objeto boton" type="button"><h3 class="objeto">Femenino</h3></button><button id="nino" class="objeto boton" type="button"><h3 class="objeto">Masculino</h3></button></div>'
+        },
+        {
+            'pag2' : '<div id="pag2" class="fondo"><h1 class="objeto titulo">Sistema reproductor Femenino.<br/>Seleccione que deseas estudiar</h1></div>'
+        },
+        {
+            'pag3' : '<div id="pag3" class="fondo"><h1 class="objeto titulo">Sistema reproductor Masculino.<br/>Seleccione que deseas estudiar</h1></div>'
         }
     ];
 });
