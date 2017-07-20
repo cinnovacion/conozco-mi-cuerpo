3bcdb2da66604f330cc5bf078de7880947838139
lindamar <linkl3@hotmail.com>
2017-07-20 17:01:00 -0600

    sistema femenino con botones

--- js/activity.js
+++ js/activity.js
@@ -1,5 +1,3 @@
-"user strict";
-
 define(function (require) {
     var activity    = require("sugar-web/activity/activity");
     var datastore   = require("sugar-web/datastore");
@@ -21,21 +19,33 @@ define(function (require) {
         $('.reload-button').on('click', function() {
             location.reload();
         });
+        
+        function mustache(id,plantilla,txt){
+            $('#canvas').on('click',id ,function(){
+            output = Mustache.render(plantilla,txt);
+            $('#canvas').html(output);
+        });
+        }
 
         $(document).ready(function(){
-            output = Mustache.render(plantilla[1].pag1, {name: "Luke"});
+            output = Mustache.render(plantilla[2].pag1);
             $('#canvas').html(output);
         });
         
-        $('#canvas').on('click','#nina',function(){
-            output = Mustache.render(plantilla[2].pag2);
-            $('#canvas').html(output);
-        });
+        for(var i=0; i<plantilla[5].length; i++){
+            mustache(plantilla[5].pl[i]);
+        }
         
-        $('#canvas').on('click','#nino',function(){
-            output = Mustache.render(plantilla[3].pag3);
-            $('#canvas').html(output);
-        });
-    
+        mustache('.btn_sel_f',plantilla[1].menu);
+        mustache("#exploro",plantilla[3].pag2);
+        mustache('#btn_ovario',plantilla[3].ovario,{text : plantilla[0].contenido.ovarios});
+        mustache('#btn_falopio',plantilla[3].falopio,{text : plantilla[0].contenido.falopio});
+        mustache('#btn_vagina',plantilla[3].vagina,{text : plantilla[0].contenido.vagina});
+        mustache('#btn_ovulo',plantilla[3].ovulo,{text : plantilla[0].contenido.ovulo});
+        mustache('#btn_uretra',plantilla[3].uretra,{text : plantilla[0].contenido.uretra});
+        mustache('#btn_utero',plantilla[3].utero,{text : plantilla[0].contenido.utero});
+        mustache(".btn_atras",plantilla[3].pag2);
+        mustache('.btn_back_menu',plantilla[2].pag1);
+        
 	});
 });
--- js/plantilla.js
+++ js/plantilla.js
@@ -2,18 +2,46 @@ define(function (require) {
     return [
         {
             'contenido' : {
-                'pag1' : 'algo mas',
-                'pag2' : 'algo mas'
-            }  
+                'ovarios' : 'Los ovarios son dos, están situados en el abdomen de la mujer; y producen los óvulos.',
+                
+                'falopio' : 'Son dos conductos que comunican a cada ovario con el útero o matriz. En las trompas de falopio se encuentran unas células              ciliadas que ayudan a transportar el óvulo hasta el útero.',
+                
+                'vagina' : 'Es el conducto que comunica el útero con el exterior, tiene propiedades elásticas. Es un órgano tubular que sirve de vía de acceso para los espermatozoides y de salida para el flujo menstrual. También es el canal por donde nace el nuevo ser.',
+                
+                'ovulo' : 'Célula sexual femenina, originada en el ovario con un ritmo de unos 28 días. Un óvulo es más grande que un espermatozoide y carece de movilidad.',
+                
+                'uretra' : 'Es un conducto que comunica la vejiga con el orificio urinario en la pared del cuerpo. La uretra es más larga en el hombre que en la mujer, ya que recorre el interior del pene. Además de servir para la evacuación de la orina, la uretra en hombre también sirve para llevar el líquido seminal.',
+                
+                'utero' : 'El útero o matriz es una cavidad musculosa situado bajo el vientre, cubierta en su parte interna por una capa de células del tejido epitelial.Este es el órgano que durante el embarazo aloja al feto, para su desarrollo.'
+               }
         },
         {
-            'pag1' : '<div id="pag1"><h1 class="objeto titulo">Seleccione el sistema reproductor<br/> que deseas estudiar</h1><button id="nina" class="objeto boton" type="button"><h3 class="objeto">Femenino</h3></button><button id="nino" class="objeto boton" type="button"><h3 class="objeto">Masculino</h3></button></div>'
+            'menu' : '<div class="fondo"><button class="btn_back_menu btn_back">Menu</button><button class="boton" id="exploro"><h3 class="objeto">Exploro</h3></button><button class="boton" id="juego"><h3 class="objeto">Juego</h3></button></div>',
+            
         },
         {
-            'pag2' : '<div id="pag2" class="fondo"><h1 class="objeto titulo">Sistema reproductor Femenino.<br/>Seleccione que deseas estudiar</h1></div>'
+            'pag1' : '<div class="fondo" id="pag1"><h1 class="objeto titulo">Seleccione el sistema reproductor<br/> que deseas estudiar</h1><button id="nina" class="objeto boton btn_sel_f" type="button"><h3 class="objeto">Femenino</h3></button><button id="nino" class="objeto boton" type="button"><h3 class="objeto">Masculino</h3></button></div>'
+        },
+        {
+            'pag2' : '<div class="fondo"><button class="btn_back btn_sel_f">Atras</button><h1 class="objeto titulo">Sistema reproductor Femenino.<br/>Seleccione que deseas estudiar</h1><div id="img_srf"><button class="btn_radius" id="btn_ovario"></button><button class="btn_radius" id="btn_falopio"></button><button class="boton btn_radius" id="btn_vagina"></button><button class="boton btn_radius" id="btn_ovulo"></button><button class="btn_radius" id="btn_uretra"></button><button class="btn_radius" id="btn_utero"></button></div></div>',
+            
+            'ovario' : '<div class="fondo"><div id="ovario"><div class="text_matrix">{{text}}</div></div><button class="btn_atras btn_back">Atras</button></div>',
+            
+            'falopio' : '<div class="fondo"><div id="falopio"><div class="text_matrix">{{text}}</div></div><button class="btn_atras btn_back">Atras</button></div>',
+            
+            'vagina' : '<div class="fondo"><div id="vagina"><div class="text_matrix">{{text}}</div></div><button class="btn_atras btn_back">Atras</button></div>',
+            
+            'ovulo' : '<div class="fondo"><div id="ovulo"><div class="text_matrix">{{text}}</div></div><button class="btn_atras btn_back">Atras</button></div>',
+            
+            'uretra' : '<div class="fondo"><div id="uretra"><div class="text_matrix">{{text}}</div></div><button class="btn_atras btn_back">Atras</button></div>',
+            
+            'utero' : '<div class="fondo"><div id="utero"><div class="text_matrix">{{text}}</div></div><button class="btn_atras btn_back">Atras</button></div>'
         },
         {
             'pag3' : '<div id="pag3" class="fondo"><h1 class="objeto titulo">Sistema reproductor Masculino.<br/>Seleccione que deseas estudiar</h1></div>'
+        },
+        {
+            'pl' : ['#nino' , 1 , "menu"]
         }
     ];
 });
