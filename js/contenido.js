310efe63d505d4cdabab2b34ba8f5d80699483e0
Samuel Gutiérrez Avilés <search.sama@gmail.com>
2017-08-15 02:16:14 -0600

    inicio de la rama de desarrollo

--- js/contenido.js
+++ js/contenido.js
@@ -4,71 +4,71 @@ define(function (require) {
         {//00
             'titulo' : 'Óvulo',
             'teoria' : 'Célula sexual femenina, originada en el ovario con un ritmo de unos 28 días.Un óvulo es más grande que un espermatozoide y carece de movilidad.',
-            'grafica' : '../img/femenino/ovulo.png'
+            'grafica' : '../img/femenino/ovulo.svg'
         },
         {//01
             'titulo' : 'Trompa de falopio',
             'teoria' : 'Son dos conductos que comunican a cada ovario con el útero o matriz. En las trompas de falopio se encuentran unas células ciliadas que ayudan a transportar el óvulo hasta el útero.',
-            'grafica' : '../img/femenino/trompas-falopio.png'
+            'grafica' : '../img/femenino/trompas-falopio.svg'
         },
         {//02
             'titulo' : 'Ovarios',
             'teoria' : 'Los ovarios son dos, están situados en el abdomen de la mujer; y producen los óvulos.',
-            'grafica' : '../img/femenino/ovarios.png'
+            'grafica' : '../img/femenino/ovarios.svg'
         },
         {//03
             'titulo' : 'Útero',
             'teoria' : 'El útero o matriz es una cavidad musculosa situado bajo el vientre, cubierta en su parte interna por una capa de células del tejido epitelial. Este es el órgano que durante el embarazo aloja al feto, para su desarrollo.',
-            'grafica' : '../img/femenino/utero.png'
+            'grafica' : '../img/femenino/utero.svg'
         },
          {//04
             'titulo' : 'Uretra',
             'teoria' : 'Es un conducto que comunica la vejiga con el orificio urinario en la pared del cuerpo.La uretra es más larga en el hombre que en la mujer, ya que recorre el interior del pene. Además de servir para la evacuación de la orina, la uretra en hombre también sirve para llevar el líquido seminal.',
-            'grafica' : '../img/femenino/uretra.png'
+            'grafica' : '../img/femenino/uretra.svg'
         },
         {//05
             'titulo' : 'Vagina',
             'teoria' : 'Es el conducto que comunica el útero con el exterior, tiene propiedades elásticas. Es un órgano tubular que sirve de vía de acceso para los espermatozoides y de salida para el flujo menstrual. También es el canal por donde nace el nuevo ser.',
-            'grafica' : '../img/femenino/vagina.png'
+            'grafica' : '../img/femenino/vagina.svg'
         },
         //sistema reproductor masculino
         {//06
             'titulo' : 'Vesículas seminales',
             'teoria' : 'Son glándulas productoras de aproximadamente el 60% del volumen del líquido seminal situadas en la excavación pélvica. Detrás de la vejiga urinaria, delante del recto e inmediatamente por encima de la base de la próstata, con la que están unidas por su extremo inferior.La misión de las vesículas seminales consiste en acoger a los espermatozoides maduros. Las vesículas seminales se encargan de fabricar un líquido viscoso, llamado porción seminal, para que los espermatozoides puedan nutrirse, protegerse y desplazarse con facilidad.',
-            'grafica' : '../img/masculino/vesicula-seminal.png'
+            'grafica' : '../img/masculino/vesicula-seminal.svg'
         },
         {//07
             'titulo' : 'Próstata',
             'teoria' : 'La próstata es una glándula del aparato urogenital masculino cuya forma visual se podría asemejar al de una nuez. La próstata está situada debajo de la vejiga, enfrente del recto y está atravesada por la uretra. De esta forma la orina sale de la vejiga, atravesando la próstata, hasta que se expulsa. Su función principal es generar el fluido en el que permanecen los espermatozoides. La próstata contiene las células que logran que se forme el líquido seminal que protege y nutre a los espermatozoides.',
-            'grafica' : '../img/masculino/prostata.png'
+            'grafica' : '../img/masculino/prostata.svg'
         },
         {//08
             'titulo' : 'Conducto deferente',
             'teoria' : 'Son un par de tubos musculares rodeados de músculo liso, cada uno de 35 a 45 cm aproximadamente, que conectan el epidídimo con los conductos eyaculatorios intermediando el recorrido del semen entre estos.',
-            'grafica' : '../img/masculino/conducto-deferente.png'
+            'grafica' : '../img/masculino/conducto-deferente.svg'
         },
-        {//09 falta la imagen en png
+        {//09 falta la imagen en svg
             'titulo' : 'Uretra',
             'teoria' : 'La uretra es una vía común para el paso de la orina y del semen, atraviesa el cuerpo esponjoso y termina en un orificio conocido con el nombre de meato urinario, el cual se encuentra en el extremo del glande.En el hombre la uretra tiene una longitud de unos doce centímetros y se abre al exterior en el meatus uretral del glande. En la mujer tiene una longitud de 3.5 cm con un diámetro de 6 mm.',
-            'grafica' : '../img/masculino/uretra.png'
+            'grafica' : '../img/masculino/uretra.svg'
         },
         {//10
             'titulo' : 'Testículos',
             'teoria' : 'Son cuerpos ovoides pares que se enuentran suspendidos en el escroto. En ellos se producete las hormanas masculinas, llamadas andrógenos. La mas importante de esta es la testosterona, que estimula la producción de espermatozoides y la diferenciación sexual masculina.',
-            'grafica' : '../img/masculino/testiculo.png'
+            'grafica' : '../img/masculino/testiculo.svg'
         },
         {//11
             'titulo' : 'Pene',
             'teoria' : 'El pene es un órgano externo. Dentro de sus funciones reproductivas se encuentra la de depositar el esperma mediante el coito en el aparato reproductor femenino con el fin de fecundar el óvulo.',
-            'grafica' : '../img/masculino/pene.png'
+            'grafica' : '../img/masculino/pene.svg'
         },
         {//12
             'titulo' : 'Escroto',
             'teoria' : 'El escroto o saco escrotal es la cubierta de piel que engloba y almacena los testículos, los vasos sanguíneos, parte del cordón espermático y las vías excretoras fuera del abdomen en los varones.',
-            'grafica' : '../img/masculino/escroto.png'
+            'grafica' : '../img/masculino/escroto.svg'
         },
         {//13
-            'grafica' : '../img/femenino/srf_juego.png',
+            'grafica' : '../img/femenino/srf_juego.svg',
             'botones' : [{ 'boton' : 'Útero' },{ 'boton': 'Trompa de Falopio' },{ 'boton': 'Ovario' },{ 'boton': 'Óvulo' },{ 'boton': 'Uretra' },{ 'boton': 'Vagina' }],
             'respuestas' : [{ 'res' : 'utero' },{ 'res': 'trompadefalopio' },{ 'res': 'ovario' },{ 'res': 'ovulo' },{ 'res': 'uretra' },{'res': 'vagina' }]
         }
